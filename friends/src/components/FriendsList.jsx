e96e1fcf65c1868e26aa64883e6df6016822d651
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-11-12 18:32:08 -0600

    making get request in <FriendsList/> and rendering data

--- friends/src/components/FriendsList.jsx
+++ friends/src/components/FriendsList.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const FriendsList = props => {
+    const [friends, setFriends] = useState([])
+
+
+    useEffect(() => {
+        axiosWithAuth()
+            .get('/friends')
+            .then(response => {
+                setFriends(response.data)
+            })
+            .catch(error => console.log(error))
+    }, [])
+
     return (
         <div className='friends-list'>
             <h1>Friend's List</h1>
+            {friends.map(friend => {
+                return (
+                    <div className='friend-card' key={friend.id}>
+                        <h4>{`${friend.name} is ${friend.age}`}</h4>
+                        <p>{`Contact him at ${friend.email}`}</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
